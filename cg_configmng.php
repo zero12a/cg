bec00f807ca68505f5c31b04b1ee4bb39863d8b3
kims man <zero12a@gmail.com>
2019-12-04 22:59:03 +0900

    lib sql parser

--- cg_configmng.php
+++ cg_configmng.php
@@ -169,7 +169,7 @@ if($_POST["CONFIG_NM"] == ""){
             ,"CFG_LIBS_SQL_PARSER" => "/data/www/lib/php/PHP-SQL-Parser/src/PHPSQLParser.php"
             ,"CFG_LIBS_HTML_PURIFIER" => "/data/www/lib/php/HTMLPurifier-4.12.0/library/HTMLPurifier.auto.php" //xss 필터
             ,"CFG_LIBS_MONO_LOG" => "/data/www/lib/php/monolog/autoload.php"
-        
+            ,"CFG_LIBS_SQL_PARSER" => "/data/www/lib/php/PHP-SQL-Parser/src/PHPSQLParser.php"
         
             ,"CFG_AWS_AID" => ""
             ,"CFG_AWS_KEY" => ""
