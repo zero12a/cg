3984ac79b0ff45c6e94028bcfe6c1f9c3d19621a
kims man <zero12a@gmail.com>
2020-03-22 21:02:15 +0900

    good

--- md/CGPJT1-CG_ICONMNG_BACKUP.sql
+++ md/CGPJT1-CG_ICONMNG_BACKUP.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: 172.17.0.1    Database: CGCORE
+-- Host: 172.17.0.1    Database: CGPJT1
 -- ------------------------------------------------------
 -- Server version	5.7.20
 
@@ -16,36 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `CG_USERS`
+-- Table structure for table `CG_ICONMNG`
 --
 
-DROP TABLE IF EXISTS `CG_USERS`;
+DROP TABLE IF EXISTS `CG_ICONMNG`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `CG_USERS` (
-  `USERSEQ` int(11) NOT NULL,
-  `EMAIL` varchar(300) NOT NULL,
-  `PASSWD` varchar(300) NOT NULL,
-  `EMAILVALIDYN` varchar(1) NOT NULL,
-  `LASTPWCHGDT` varchar(14) NOT NULL,
-  `PWFAILCNT` int(11) NOT NULL DEFAULT '0',
-  `LOCKYN` varchar(1) NOT NULL,
-  `FREEZEDT` varchar(14) NOT NULL,
-  `LOCKDT` varchar(14) NOT NULL,
-  `SERVERSEQ` int(11) NOT NULL,
+CREATE TABLE `CG_ICONMNG` (
+  `ICONSEQ` int(11) NOT NULL AUTO_INCREMENT,
+  `IMGNM` varchar(100) NOT NULL,
+  `IMGSVRNM` varchar(100) NOT NULL,
+  `IMGSIZE` int(11) NOT NULL,
+  `IMGHASH` varchar(100) DEFAULT NULL,
+  `IMGTYPE` int(11) DEFAULT NULL,
   `ADDDT` varchar(14) NOT NULL,
-  `MODDT` varchar(14) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`ICONSEQ`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `CG_USERS`
+-- Dumping data for table `CG_ICONMNG`
 --
 
-LOCK TABLES `CG_USERS` WRITE;
-/*!40000 ALTER TABLE `CG_USERS` DISABLE KEYS */;
-INSERT INTO `CG_USERS` VALUES (1,'zero12a@gmail.com','3e453f868136a31be81a1923f4c4011ce83355b26b76834f0424dabfb3611b4784d9bcfb290831c3b0446ebfd42dcf2621ebdef6d1fee51c63a35e7ff7d75299','Y','20180122224831',0,'N','','',3,'20160415111111','20180122225108');
-/*!40000 ALTER TABLE `CG_USERS` ENABLE KEYS */;
+LOCK TABLES `CG_ICONMNG` WRITE;
+/*!40000 ALTER TABLE `CG_ICONMNG` DISABLE KEYS */;
+INSERT INTO `CG_ICONMNG` VALUES (1,'crypt_lock.png','PIC_200320071434afX3.png',379,NULL,NULL,'20200320071434'),(2,'reload.png','PIC_200320071529P9RG.png',54667,NULL,NULL,'20200320071529'),(3,'reload.png','PIC_2003200728348yPf.png',54667,NULL,NULL,'20200320072834'),(4,'reload.png','PIC_200320073050el1U.png',54667,NULL,NULL,'20200320073050'),(5,'reload.png','PIC_200320073157pXrc.png',54667,NULL,NULL,'20200320073157'),(6,'reload.png','PIC_200320073334LZaK.png',54667,NULL,NULL,'20200320073334'),(7,'crypt_lock.png','PIC_200321213419EcIg.png',379,'20d12cd2d4c62191d6c4666b0ff11daab6d27b8a50d4daad4491451ab4a4c2d5',3,'20200321213419');
+/*!40000 ALTER TABLE `CG_ICONMNG` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -57,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-22 21:01:48
+-- Dump completed on 2020-03-21 22:40:09
